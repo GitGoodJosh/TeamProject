4aac7eed8691081f8ea0e9601daf7b095fc7d845
AryaVatsa <arya.vatsa@icloud.com>
2023-08-04 21:09:19 +0800

    move

--- pygamenew.py
+++ pygamenew.py
@@ -1,5 +1,7 @@
 import os
+import time
 import pygame
+import pygameClasses
 from pygameClasses import tank
 from pygameClasses import fighter
 pygame.init()
@@ -15,54 +17,86 @@ BGimage = pygame.image.load(os.path.join('Assets','pygameBACKGROUND.png')).conve
 TANKimage = pygame.image.load(os.path.join('Assets','pygameTANK.png')).convert_alpha()
 FIGHTERimage = pygame.image.load(os.path.join('Assets','pygameFIGHTER.png')).convert_alpha()
 HEALERimage = pygame.image.load(os.path.join('Assets','pygameHEALER.png')).convert_alpha()
+FIGHTERimageAI = pygame.transform.flip(FIGHTERimage, True, False)
+TANKimageAI = pygame.transform.flip(TANKimage, True, False)
+HEALERimageAI = pygame.transform.flip(HEALERimage, True, False)
 # load all images before this line
 # ------------------------------------------------- #
-# T is first tank F is first fighter
-T = tank(400, 400, 200, 30, 20)
-F = fighter(600, 600, 150, 50, 10)
+## T is first tank F is first fighter
+# T = tank(400, 400, 200, 30, 20)
+# F = fighter(600, 600, 150, 50, 10)
 
 
+    
 
 
+# print BG and characters
+screen = pygame.display.get_surface()
+screen.blit(BGimage, (0,0))
+def setup(exception):
+    if exception == FIGHTERimage:   
+        screen.blit(BGimage, (0,0))
+        screen.blit(TANKimage, (100,300))
+        screen.blit(HEALERimage, (0,450))
+        screen.blit(FIGHTERimageAI, (850,150))
+        screen.blit(TANKimageAI, (750,300))
+        screen.blit(HEALERimageAI, (870,450))
+    elif exception == TANKimage:
+        screen.blit(BGimage, (0,0))
+        screen.blit(FIGHTERimage, (0,150))
+        screen.blit(HEALERimage, (0,450))
+        screen.blit(FIGHTERimageAI, (850,150))
+        screen.blit(TANKimageAI, (750,300))
+        screen.blit(HEALERimageAI, (870,450))
+        
+
+pygame.display.flip()
 
 
+def moveFighter(CHARACTERx, CHARACTERy, targetX, targetY, CHARACTERimage = FIGHTERimage):
 
 
+    originalX = CHARACTERx
+    originalY = CHARACTERy
 
+    for i in range(50):
+        CHARACTERx = CHARACTERx + (targetX - originalX)/50
+        CHARACTERy = CHARACTERy + (targetY - originalY)/50
+        screen.blit(CHARACTERimage, (CHARACTERx, CHARACTERy))
+        pygame.display.flip()
+        time.sleep(0.05)
+        setup(FIGHTERimage)
 
-# print BGimage
-pygame.display.get_surface().blit(BGimage, (0,0))
-pygame.display.get_surface().blit(FIGHTERimage, (0,150))
-pygame.display.get_surface().blit(TANKimage, (100,300))
-pygame.display.get_surface().blit(HEALERimage, (0,450))
-# main loop to keep window open (pygame.QUIT is the event type when the cross is pressed)
-running = True 
-while running == True:
+    screen.blit(CHARACTERimage, (originalX, originalY))
     pygame.display.flip()
+
+
+            
+
+
+
+            
+
+
+    
+
+clock = pygame.time.Clock()
+running = True 
+# main loop to keep window open (pygame.QUIT is the event type when the cross is pressed)
+while running == True:
+    moveFighter(0, 150, 700, 300)
+    pygame.display.flip()
+    
+
+
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
 
+
 # required to uninitialise unnecessary resources if running game as a part of a larger program 
-pygame.quit
+pygame.quit()
 
-
-'''
-# testing fighter take dmg from tank hp = 150 - (30 - 10)
-print(F.takeDMG(T.applyDMG()))
-# yes
-#testing if tank takes dmg properly output should be 200 - (50 - 20) = 170
-print(T.takeDMG(F.applyDMG()))
-#yes
-# test if tank updates self.hp after taking hit (should show 170, 140, 110, 80...)
-print(T.takeDMG(F.applyDMG()))
-print(T.takeDMG(F.applyDMG()))
-print(T.takeDMG(F.applyDMG()))
-
-# yes
-#test heal (1/4 of lost hp)
-T.healHP()
-print(T.hpCurrent)
-# yes
-'''
