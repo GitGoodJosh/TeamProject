5cf1892edd1febfd743bd85f20663c1d0a04b292
AryaVatsa <arya.vatsa@icloud.com>
2023-08-04 21:06:52 +0800

    fighter movement

--- pygameClasses.py
+++ pygameClasses.py
@@ -1,5 +1,8 @@
+import pygame
+
+
 # define all classes here
-class character:
+class character(pygame.sprite.Sprite):
     def __init__(self, x, y, maxHp, Attack, Defense):
         self.xCoord = x
         self.yCoord = y 
@@ -7,6 +10,8 @@ class character:
         self.hpCurrent = maxHp
         self.attack = Attack
         self.defense = Defense
+    def move(self):
+        self.position = (self.xCoord + 10, self.yCoord + 10)
     def applyDMG(self):
         return self.attack
     def takeDMG(self, attackApplied):
@@ -27,7 +32,29 @@ class fighter(character):
         super().__init__(x, y, maxHp, attack, defense)
 
 
+class cleric(character):
+    def __init__(self, x, y, maxHp, attack, defense):
+        super().__init__(x, y, maxHp, attack, defense)
 
 
 
 
+
+''' use test in pygame file dont run here
+# testing fighter take dmg from tank hp = 150 - (30 - 10)
+print(F.takeDMG(T.applyDMG()))
+# yes
+#testing if tank takes dmg properly output should be 200 - (50 - 20) = 170
+print(T.takeDMG(F.applyDMG()))
+#yes
+# test if tank updates self.hp after taking hit (should show 170, 140, 110, 80...)
+print(T.takeDMG(F.applyDMG()))
+print(T.takeDMG(F.applyDMG()))
+print(T.takeDMG(F.applyDMG()))
+
+# yes
+#test heal (1/4 of lost hp)
+T.healHP()
+print(T.hpCurrent)
+# yes
+'''
