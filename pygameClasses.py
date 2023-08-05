4604852518556a6152d8c0b8904a9f3e2e9242b0
AryaVatsa <arya.vatsa@icloud.com>
2023-08-05 14:41:36 +0800

    finish movement, created instances for characters

--- pygameClasses.py
+++ pygameClasses.py
@@ -2,7 +2,7 @@ import pygame
 
 
 # define all classes here
-class character(pygame.sprite.Sprite):
+class character():
     def __init__(self, x, y, maxHp, Attack, Defense):
         self.xCoord = x
         self.yCoord = y 
@@ -10,15 +10,17 @@ class character(pygame.sprite.Sprite):
         self.hpCurrent = maxHp
         self.attack = Attack
         self.defense = Defense
-    def move(self):
-        self.position = (self.xCoord + 10, self.yCoord + 10)
     def applyDMG(self):
         return self.attack
     def takeDMG(self, attackApplied):
         self.hpCurrent = self.hpCurrent - (attackApplied - self.defense)
         return self.hpCurrent
     def healHP(self):
-        self.hpCurrent = self.hpCurrent + (1/4)*(self.hpMax - self.hpCurrent)  
+        self.hpCurrent = self.hpCurrent + (1/4)*(self.hpMax - self.hpCurrent) 
+    def xCoordOf(self):
+        return self.xCoord
+    def yCoordOf(self):
+        return self.yCoord
         
 
 # tank and assassin are child classes of character so we dont have to keep defining functions (appliedDMG/takeDMG)
@@ -34,27 +36,26 @@ class fighter(character):
 
 class cleric(character):
     def __init__(self, x, y, maxHp, attack, defense):
-        super().__init__(x, y, maxHp, attack, defense)
+        super().__init__( x, y, maxHp, attack, defense)
 
 
 
 
 
-''' use test in pygame file dont run here
-# testing fighter take dmg from tank hp = 150 - (30 - 10)
-print(F.takeDMG(T.applyDMG()))
+''' # testing fighter take dmg from tank hp = 150 - (30 - 10)
+print(playerFighter.takeDMG(AITank.applyDMG()))
 # yes
 #testing if tank takes dmg properly output should be 200 - (50 - 20) = 170
-print(T.takeDMG(F.applyDMG()))
+print(AITank.takeDMG(playerFighter.applyDMG()))
 #yes
 # test if tank updates self.hp after taking hit (should show 170, 140, 110, 80...)
-print(T.takeDMG(F.applyDMG()))
-print(T.takeDMG(F.applyDMG()))
-print(T.takeDMG(F.applyDMG()))
+print(AITank.takeDMG(playerFighter.applyDMG()))
+print(AITank.takeDMG(playerFighter.applyDMG()))
+print(AITank.takeDMG(playerFighter.applyDMG()))
 
 # yes
 #test heal (1/4 of lost hp)
-T.healHP()
-print(T.hpCurrent)
-# yes
+AITank.healHP()
+print(AITank.hpCurrent)
+# yes 
 '''
--- pygamenew.py
+++ pygamenew.py
@@ -1,9 +1,7 @@
 import os
 import time
 import pygame
-import pygameClasses
-from pygameClasses import tank
-from pygameClasses import fighter
+from pygameClasses import cleric, tank, fighter, character
 pygame.init()
 # need list for pygame height and width (cannot usse two separate numbers)
 screensize = (1000, 600)
@@ -16,8 +14,8 @@ pygame.display.set_caption(" GAME ")
 def LoadImg(img):
     return pygame.image.load(os.path.join(Asset_dir,img)).convert_alpha()
 
-def FlipImg(img, Ver:bool, Hori:bool):
-    return pygame.transform.flip(img,Ver,Hori)
+def FlipImg(img, verticalFlip:bool, horizontalFlip:bool):
+    return pygame.transform.flip(img, verticalFlip, horizontalFlip)
 
 # load all images after this line
 Asset_dir = os.path.join(os.path.dirname(__file__), 'Assets')
@@ -25,29 +23,29 @@ BGimage = LoadImg('pygameBACKGROUND.png')
 TANKimage = LoadImg('pygameTANK.png')
 FIGHTERimage = LoadImg('pygameFIGHTER.png')
 HEALERimage = LoadImg('pygameHEALER.png')
-FIGHTERimageAI = FlipImg(TANKimage,True,False)
+FIGHTERimageAI = FlipImg(FIGHTERimage,True,False)
 TANKimageAI = FlipImg(TANKimage, True, False)
 HEALERimageAI = FlipImg(HEALERimage, True, False)
-"""
-FIGHTERimageAI = pygame.transform.flip(FIGHTERimage, True, False)
-TANKimageAI = pygame.transform.flip(TANKimage, True, False)
-HEALERimageAI = pygame.transform.flip(HEALERimage, True, False)
-"""
+
+
 
 
 # load all images before this line
 # ------------------------------------------------- #
-## T is first tank F is first fighter
-# T = tank(400, 400, 200, 30, 20)
-# F = fighter(600, 600, 150, 50, 10)
 
+playerFighter = fighter(0, 150, 150, 50, 10)
+playerTank = tank(100, 300, 200, 30, 20)
+playerCleric = cleric(0, 450, 120, 20, 10)
+AIFighter = fighter(850, 150, 150, 50, 10)
+AITank = tank(750, 300, 200, 30, 20)
+AICleric = cleric(870, 450, 120, 20, 10)
 
 
 
 # print BG and characters
 screen = pygame.display.get_surface()
 screen.blit(BGimage, (0,0))
-def setup(exception):
+def blitAllexcept(exception):
     if exception == FIGHTERimage:   
         screen.blit(BGimage, (0,0))
         screen.blit(TANKimage, (100,300))
@@ -55,6 +53,7 @@ def setup(exception):
         screen.blit(FIGHTERimageAI, (850,150))
         screen.blit(TANKimageAI, (750,300))
         screen.blit(HEALERimageAI, (870,450))
+        
     elif exception == TANKimage:
         screen.blit(BGimage, (0,0))
         screen.blit(FIGHTERimage, (0,150))
@@ -62,31 +61,79 @@ def setup(exception):
         screen.blit(FIGHTERimageAI, (850,150))
         screen.blit(TANKimageAI, (750,300))
         screen.blit(HEALERimageAI, (870,450))
+
+    elif exception == HEALERimage:
+        screen.blit(BGimage, (0,0))
+        screen.blit(FIGHTERimage, (0,150))
+        screen.blit(TANKimage, (100,300))
+        screen.blit(FIGHTERimageAI, (850,150))
+        screen.blit(TANKimageAI, (750,300))
+        screen.blit(HEALERimageAI, (870,450))
+
+    elif exception == FIGHTERimageAI:
+        screen.blit(BGimage, (0,0))
+        screen.blit(FIGHTERimage, (0,150))
+        screen.blit(TANKimage, (100,300))
+        screen.blit(HEALERimage, (0,450))
+        screen.blit(TANKimageAI, (750,300))
+        screen.blit(HEALERimageAI, (870,450))
+
+
+    elif exception == TANKimageAI:
+        screen.blit(BGimage, (0,0))
+        screen.blit(FIGHTERimage, (0,150))
+        screen.blit(TANKimage, (100,300))
+        screen.blit(HEALERimage, (0,450))
+        screen.blit(FIGHTERimageAI, (850,150))
+        screen.blit(HEALERimageAI, (870,450))
+
+    elif exception == HEALERimageAI:
+        screen.blit(BGimage, (0,0))
+        screen.blit(FIGHTERimage, (0,150))
+        screen.blit(TANKimage, (100,300))
+        screen.blit(HEALERimage, (0,450))
+        screen.blit(FIGHTERimageAI, (850,150))
+        screen.blit(TANKimageAI, (750,300))
         
 
 pygame.display.flip()
 
 
-def moveFighter(CHARACTERx, CHARACTERy, targetX, targetY, CHARACTERimage = FIGHTERimage):
+def move(CHARACTERx, CHARACTERy, targetX, targetY, CHARACTERimage):
 
 
     originalX = CHARACTERx
     originalY = CHARACTERy
 
-    for i in range(50):
-        CHARACTERx = CHARACTERx + (targetX - originalX)/50
-        CHARACTERy = CHARACTERy + (targetY - originalY)/50
+    for i in range(30):
+        CHARACTERx = CHARACTERx + (targetX - originalX)/30
+        CHARACTERy = CHARACTERy + (targetY - originalY)/30
         screen.blit(CHARACTERimage, (CHARACTERx, CHARACTERy))
         pygame.display.flip()
-        time.sleep(0.05)
-        setup(FIGHTERimage)
+        time.sleep(0.01)
+        blitAllexcept(CHARACTERimage)
 
     screen.blit(CHARACTERimage, (originalX, originalY))
     pygame.display.flip()
 
+'''
+# testing fighter take dmg from tank hp = 150 - (30 - 10)
+print(playerFighter.takeDMG(AITank.applyDMG()))
+# yes
+#testing if tank takes dmg properly output should be 200 - (50 - 20) = 170
+print(AITank.takeDMG(playerFighter.applyDMG()))
+#yes
+# test if tank updates self.hp after taking hit (should show 170, 140, 110, 80...)
+print(AITank.takeDMG(playerFighter.applyDMG()))
+print(AITank.takeDMG(playerFighter.applyDMG()))
+print(AITank.takeDMG(playerFighter.applyDMG()))
 
-            
-
+# yes
+#test heal (1/4 of lost hp)
+AITank.healHP()
+print(AITank.hpCurrent)
+# yes            
+'''
 
 
             
@@ -98,7 +145,7 @@ clock = pygame.time.Clock()
 running = True 
 # main loop to keep window open (pygame.QUIT is the event type when the cross is pressed)
 while running == True:
-    moveFighter(0, 150, 700, 300)
+    move(playerFighter.xCoordOf(), playerFighter.yCoordOf(), AITank.xCoordOf(), AITank.yCoordOf(), FIGHTERimage)
     pygame.display.flip()
     
 
